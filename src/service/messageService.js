6743bac875369dfab5f557f66c36f6b4a2e38b7b
TranMinhQuang2k2 <quangminhtranq@gmail.com>
2023-06-24 22:51:37 +0700

    fix bug

--- src/service/messageService.js
+++ src/service/messageService.js
@@ -53,35 +53,40 @@ export const getAllRoomChat = async (req, res) => {
 
       let userName;
       let guestName;
-      if (idLists.length !== 0) {
-        if (idLists[0].dataValues.userId === user.dataValues.id) {
+      const obj = [];
+
+      for (let i = 0; i < idLists.length; i++) {
+        if (idLists[i].dataValues.userId === user.dataValues.id) {
           guestName = await db.User.findOne({
             where: {
-              id: idLists[0].dataValues.sellerId,
+              id: idLists[i].dataValues.sellerId,
             },
             attributes: ["name"],
           });
           userName = user.dataValues.name;
+
+          obj.push({
+            ...idLists[i].dataValues,
+            userName,
+            guestName: guestName.dataValues.name,
+          });
         }
-        if (idLists[0].dataValues.sellerId === user.dataValues.id) {
+        if (idLists[i].dataValues.sellerId === user.dataValues.id) {
           guestName = await db.User.findOne({
             where: {
-              id: idLists[0].dataValues.userId,
+              id: idLists[i].dataValues.userId,
             },
             attributes: ["name"],
           });
           userName = user.dataValues.name;
+
+          obj.push({
+            ...idLists[i].dataValues,
+            userName,
+            guestName: guestName.dataValues.name,
+          });
         }
       }
-
-      const obj = [];
-      idLists.forEach((item) => {
-        obj.push({
-          ...item.dataValues,
-          userName,
-          guestName: guestName.dataValues.name,
-        });
-      });
 
       const response = responseWithJWT(req, obj, user);
       res.status(200).json(response);
