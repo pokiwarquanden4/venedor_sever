314799159e163b5874de565fa43ad19b0bd045be
TranMinhQuang2k2 <quangminhtranq@gmail.com>
2023-06-25 15:32:56 +0700

    fix bug

--- src/service/socket/socketMessage.js
+++ src/service/socket/socketMessage.js
@@ -1,18 +1,25 @@
 import { createMessage } from "../messageService";
 
 const socketConnection = (io) => {
-  io.on("connection", (socket) => {
-    socket.on("join_room", (data) => {
-      socket.join(data);
+  try {
+    io.on("connection", (socket) => {
+      socket.on("join_room", (data) => {
+        console.log(socket.id + " Join to room: " + data);
+        socket.join(data);
+      });
+      socket.on("send_message", (data) => {
+        console.log("Sent to room: " + data.roomId);
+        socket.to(data.roomId).emit(`receive_message`, data);
+        createMessage(data);
+      });
+      socket.on("exit_room", (data) => {
+        console.log(socket.id + " Leave room: " + data);
+        socket.leave(data);
+      });
     });
-    socket.on("send_message", (data) => {
-      socket.to(data.roomId).emit(`receive_message`, data);
-      createMessage(data);
-    });
-    socket.on("exit_room", (data) => {
-      socket.leave(data);
-    });
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default socketConnection;
